2c64d0da8a673ea5995a61992efc0af050af0c47
Cierra <ceceminorhampton@gmail.com>
2023-04-17 16:47:56 -0400

    finally finished

---
+++ Editing-DOM/Editing-DOM.js
@@ -0,0 +1,25 @@
+const li = document.querySelectorAll('li');
+// console.log(li[1].innerHTML); //shows Apples (before the text change)
+li[1].innerHTML = "Granny Smith Apples";
+// console.log(li[1].innerHTML);
+
+document.querySelector('.oatM').remove();
+
+const newLi = document.createElement('li');
+newLi.innerHTML = "Kombucha";
+const addedList = document.querySelector('ul');
+addedList.appendChild(newLi);
+
+const newValues = ['protein bar, almonds, peanut butter'];
+const list = document.getElementById("list");
+// const list = document.querySelector('#list');
+list.innerHTML = "";
+
+const items = ["protein bars", "almonds", "peanut butter"];
+      items.forEach((item) => {
+        const li = document.createElement("li");
+        li.textContent = item;
+        list.appendChild(li);
+      });
+
+      list.children[1].classList.add("important");
