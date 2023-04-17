179eb47a42573b2dd6d3f789ba81d4396d75eb4b
Cierra <ceceminorhampton@gmail.com>
2023-04-17 14:29:30 -0400

    Finished

---
+++ T-T-DOM/Traversing-the-DOM.js
@@ -0,0 +1,20 @@
+const header = document.getElementsByTagName('header');
+console.log(header);
+
+const section = document.getElementsByTagName('section');
+console.log(section);
+
+const current = document.getElementsByClassName('current');
+console.log(current);
+
+const sections = document.getElementsByTagName('sections');
+console.log(section[2]);
+
+const h2 = document.querySelector('h2');
+console.log(h2);
+// number6
+const div = document.querySelector('h2.highlight');
+console.log(div);
+
+const sectionH2 = document.querySelectorAll('h2');
+console.log(sectionH2);
