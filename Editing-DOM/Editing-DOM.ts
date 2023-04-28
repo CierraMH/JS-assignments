a4e163127b77b1ab12002920de6a3d58f120878f
Cierra <ceceminorhampton@gmail.com>
2023-04-27 20:11:52 -0400

    changed the .js to .ts and made a new .js

--- Editing-DOM/Editing-DOM.ts
+++ Editing-DOM/Editing-DOM.ts
@@ -1,21 +1,25 @@
-"use strict";
-const li = document.querySelectorAll('li');
+const li: NodeListOf<HTMLElement> = document.querySelectorAll('li');
 // console.log(li[1].innerHTML); //shows Apples (before the text change)
 li[1].innerHTML = "Granny Smith Apples";
 // console.log(li[1].innerHTML);
-document.querySelector('.oatM').remove();
+
+document.querySelector<any>('.oatM').remove();
+
 const newLi = document.createElement('li');
 newLi.innerHTML = "Kombucha";
-const addedList = document.querySelector('ul');
+const addedList: any = document.querySelector('ul');
 addedList.appendChild(newLi);
+
 const newValues = ['protein bar, almonds, peanut butter'];
-const list = document.getElementById("list");
+const list: any = document.getElementById("list");
 // const list = document.querySelector('#list');
 list.innerHTML = "";
+
 const items = ["protein bars", "almonds", "peanut butter"];
-items.forEach((item) => {
-    const li = document.createElement("li");
-    li.textContent = item;
-    list.appendChild(li);
-});
-list.children[1].classList.add("important");
+      items.forEach((item) => {
+        const li = document.createElement("li");
+        li.textContent = item;
+        list.appendChild(li);
+      });
+
+      list.children[1].classList.add("important");
